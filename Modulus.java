486d990854abe91a2076050003a5bba73c18d46e
Ayyappa19 <119914633+Ayyappa19@users.noreply.github.com>
2023-08-04 21:12:21 +0530

    import java.util.Scanner; public class Modules{     public static void main(String args[])     {         Scanner m = new Scanner(System.in);         int a,b,c;         a=m.nextInt();         b=m.nextInt();         c=a%b;         System.out.println(c);     } }

--- Modulus.java
+++ Modulus.java
@@ -1,13 +1,13 @@
+
 import java.util.Scanner;
-public class Main{
-    public static void main (String args[])
+public class Modules{
+    public static void main(String args[])
     {
-        Scanner sc=new Scanner(System.in);
-        int a,b,c,cap;
-        a=sc.nextInt();
-        b=sc.nextInt();
-        // c=sc.nextInt();
-        cap=a%b;
-        System.out.println(cap);
+        Scanner m = new Scanner(System.in);
+        int a,b,c;
+        a=m.nextInt();
+        b=m.nextInt();
+        c=a%b;
+        System.out.println(c);
     }
 }
