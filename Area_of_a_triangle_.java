5a7343d4ca3ca3988c11bfac9f462fcb822bbcd1
Ayyappa19 <119914633+Ayyappa19@users.noreply.github.com>
2023-08-07 21:51:25 +0530

    import java.util.Scanner; public class tri{     public static void main(String args[])     {         Scanner v= new Scanner(System.in);         int a,b,c;         a=v.nextInt();         b=v.nextInt();         c=(a*b)/2;         System.out.println(c);     } }

---
+++ Area_of_a_triangle_.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class tri{
+    public static void main(String args[])
+    {
+        Scanner v= new Scanner(System.in);
+        int a,b,c;
+        a=v.nextInt();
+        b=v.nextInt();
+        c=(a*b)/2;
+        System.out.println(c);
+    }
+}
