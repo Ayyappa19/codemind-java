96a6b559f1e6324ac549961757757691431854c9
Ayyappa19 <119914633+Ayyappa19@users.noreply.github.com>
2023-09-04 14:50:47 +0530

    import java.util.*; public class fhf{     public static void main(String args[]){         Scanner x=new Scanner(System.in);         int r=x.nextInt();         int c=x.nextInt();         int a[][]= new int[r][c];         int se=0,so=0;         for (int i =0;i<r;i++)         {             for(int j=0;j<c;j++)             {                 a[i][j]=x.nextInt();                 if(i%2==0){                     se+=a[i][j];                 }                 else{                     so+=a[i][j];                 }             }         }         System.out.print(se+" "+so);     } }

---
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class fhf{
+    public static void main(String args[]){
+        Scanner x=new Scanner(System.in);
+        int r=x.nextInt();
+        int c=x.nextInt();
+        int a[][]= new int[r][c];
+        int se=0,so=0;
+        for (int i =0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                a[i][j]=x.nextInt();
+                if(i%2==0){
+                    se+=a[i][j];
+                }
+                else{
+                    so+=a[i][j];
+                }
+            }
+        }
+        System.out.print(se+" "+so);
+    }
+}
